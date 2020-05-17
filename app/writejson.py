a33ef811e5b3d2afd1ffccc11dd89419d3b977d8
Mattwalte <60742347+Mattwalte@users.noreply.github.com>
2020-05-16 20:07:32 -0400

    Update writejson.py

--- app/writejson.py
+++ app/writejson.py
@@ -1,36 +1,10 @@
 import json     # Libary provides methods to convert dict to and from json
-import datetime # Used for timestamp
-import random # I just used this to generate mock data
+import atexit   # Allows a function to be called upon exit. Used to write json file
 
-# Dictionary that will hold all sensor data. Converted to json later
-# The array portion holds new readings
-
-sensor_dict = { "sensor_data": [] }
-
-# Measurements are taken every second during the duration of 'airtime'.
-# For the sake of demonstration, this will add mock readings 4 times
-for x in range(0,4):
-
-    # Sensors will input  readings into these variables
-    latitude = random.uniform(-90, 90)
-    longitude = random.uniform(-180, 180)
-    altitude = random.randint(0, 39500)
-    roll = random.uniform(-180, 180)
-    pitch = random.uniform(-90, 90)
-    yaw = random.uniform(-180, 180)
-    ext_temp = random.uniform(-40, 80)
-    ext_humidity = random.uniform(0, 1)
-    int_temp = random.uniform(-40, 80)
-    accelerometer = [ # array of x, y, z
-        random.uniform(-10, 10), random.uniform(-10, 10), random.uniform(-10, 10)
-    ]
-    magnetometer = [ # array of x, y, z
-        random.uniform(-10, 10), random.uniform(-10, 10), random.uniform(-10, 10)
-    ]
-    gyroscope = [ # array of x, y, z
-        random.uniform(-10, 10), random.uniform(-10, 10), random.uniform(-10, 10)
-    ]
-
+# Function that takes sensor readings as arguments, and appends them to python dict
+# assumes dictionary has already been initialized prior to call
+#   i.e. this line -> sensor_dict = { "sensor_data": [] }
+def add_json():
     # Append data to the array
     sensor_dict["sensor_data"].append({
         "timestamp": datetime.datetime.now().__str__(),
@@ -56,45 +30,44 @@ for x in range(0,4):
                 "temperature-range": "\"-40 to 80°C\"",
                 "temperature-accuracy": "\"±0.5°C\""
                 },
-            "temperature": ext_temp,
-            "humidity": ext_humidity
+            "temperature": externalReading
         },
         "internal": {
             "sensor-specs": "LSM9DS1, DHT11, or DHT22",
-            "temperature": int_temp
+            "temperature_1": internalTemp1,
+            "temperature_2": internalTemp2,
+            "humidity": internalHumidity
         },
         "accelerometer": {
             "sensor-specs": "\"LSM9DS1\"",
             "def-specs": "\"A 3-tuple of X, Y, Z axis accelerometer values in meters/second squared.\"",
             "range-specs": "\"±2/±4/±8/±16 g\"",
-            "x": accelerometer[0],
-            "y": accelerometer[1],
-            "z": accelerometer[2]
+            "x": accel_x,
+            "y": accel_y,
+            "z": accel_z
         },
         "magnetometer": {
             "sensor-specs": "\"LSM9DS1\"",
             "def-specs": "\"A 3-tuple of X, Y, Z axis magnetometer values in gauss.\"",
             "range-specs": "\"±4/±8/±12/±16 gauss\"",
-            "x": magnetometer[0],
-            "y": magnetometer[1],
-            "z": magnetometer[2]
+            "x": mag_x,
+            "y": mag_y,
+            "z": mag_z
         },
         "gyroscope": {
             "sensor-specs": "\"LSM9DS1\"",
             "def-specs": "\"A 3-tuple of X, Y, Z axis gyroscope values in degrees/second.\"",
             "range-specs": "\"±245/±500/±2000 dps\"",
-            "x": gyroscope[0],
-            "y": gyroscope[1],
-            "z": gyroscope[2]
+            "x": gyro_x,
+            "y": gyro_y,
+            "z": gyro_z
         }
     })
 
-## This would convert dictinary into json string
-# string =json.dumps()
 
-## You can also print it to terminal
-# print(json.dumps())
-
-## This would create json file
-with open('generate.json', 'w') as json_file:
-    json.dump(sensor_dict, json_file,  indent = 4)
+# This should execute upon normal termination (including ctrl+c press)
+@atexit.register
+def write_json():
+    # This creates a json file using the data filled dictionary
+    with open('sensor.json', 'w') as json_file:
+        json.dump(sensor_dict, json_file,  indent = 4)
