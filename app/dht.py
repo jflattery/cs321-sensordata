dcf762978d090a3520c0e2c85c64888d3e497a90
jFlatz <6561797+jflattery@users.noreply.github.com>
2020-05-18 22:56:29 +0100

    fixes to dht sensors

--- app/dht.py
+++ app/dht.py
@@ -5,34 +5,18 @@ import board
 import adafruit_dht
 
 # Initial the dht device, with data pin connected to:
-# dhtDevice = adafruit_dht.DHT11(board.D17)
-
-# def get():
-#    temperature_c = dhtDevice.temperature
-#    humidity = dhtDevice.humidity
-#    return temperature_c, humidity
+dht11Device = adafruit_dht.DHT11(board.D17)
+dht22Device = adafruit_dht.DHT22(board.D4)
 
 def get(sensor):
     if sensor == 11:
-        try:
-          dht_device = adafruit_dht.dht11(board.D17)
-        except:
-          return float('nan')
+        temperature_c = dht11Device.temperature
+        humidity = dht11Device.humidity
     elif sensor == 22:
-        try:
-          dht_device = adafruit_dht.dht22(board.D4)
-        except:
-          return float('nan')
+        temperature_c = dht22Device.temperature
+        humidity = dht22Device.humidity
     else:
+        print("Invalid Sensor number provided")
         return float('-inf')
-
-    try:
-        temperature_c = dht_device.temperature
-    except:
-        temperature_c = float('-inf')
-    try:
-        humidity = dht_device.humidity
-    except:
-        humidity = float('-inf')
 
     return temperature_c, humidity
